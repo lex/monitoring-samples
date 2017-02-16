92555ed276301142aaab862b879a2bb4a486d284
Niko Cankar <niko.cankar@gmail.com>
2017-02-16 16:37:32 +0200

    two additional tests

--- appium-safari/java/src/test/java/com/bitbar/monitoring/samples/ios/SafariSimulatorTest.java
+++ appium-safari/java/src/test/java/com/bitbar/monitoring/samples/ios/SafariSimulatorTest.java
@@ -6,8 +6,14 @@ import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.TimeoutException;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,10 +25,11 @@ import java.net.URL;
  */
 public class SafariSimulatorTest {
 
-    private WebDriver driver;
-
-    @Before
-    public void setUp() throws Exception {
+    private static IOSDriver driver;
+    private static int picNo = 0;
+    
+    @BeforeClass
+    public static void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName", "iPhone 5s");
         capabilities.setCapability("platformName", "iOS");
@@ -30,22 +37,54 @@ public class SafariSimulatorTest {
         capabilities.setCapability("browserName", "safari");
         driver = new IOSDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"),
                 capabilities);
-        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        // open the test web page
+        driver.get("http://bitbar.github.io/testdroid-samples/");
     }
 
     @Test
-    public void runTest() throws Exception {
-        driver.get("http://bitbar.github.io/testdroid-samples/");
-
-        System.out.println("Taking screenshot...");
-
-        takeScreenshot("screenshot.png");
+    public void testClickButtonPresent() throws Exception {
+        System.out.println("Taking screenshot of sample web app");
+        waitButtonText("Click for answer");
+        takeScreenshot("samples-page.png");
     }
 
+    @Test
+    public void testCurrentUrl() throws Exception {
+        String URL = driver.getCurrentUrl();
+        System.out.println("Taking screenshot of current web page");
+        takeScreenshot("bitbarcom.png");
+        assertEquals(URL.contains("testdroid-samples"), true);
+    }
+
+    @Test
+    public void testButtonAction() throws Exception {
+        System.out.println("Screenshot before test");
+        takeScreenshot("not_clicked.png");
+        String buttonText = "Click for answer";
+        waitButtonText(buttonText);
+        // button is present click it
+        driver.findElement(By.xpath("//button[text()='"+ buttonText +"']")).click();
+        takeScreenshot("button_clicked.png");
+        driver.findElement(By.xpath("//*[@id='result_element' and contains(., 'Testdroid real device cloud')]"));
+    }
+
+
+    private void waitButtonText(String buttonText) throws TimeoutException {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 40);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='"+ buttonText +"']")));
+        } catch (TimeoutException e) {
+            System.out.println("---> Button text not found in time! Taking additional screenshot.");
+            String picName = buttonText.replace(' ', '_');
+            takeScreenshot("error_button_not_found_"+ picName +".png");
+            throw e;
+        }
+    }
+    
     private void takeScreenshot(String name) {
         try {
             File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            File testScreenshot = new File("screenshots",name);
+            File testScreenshot = new File("screenshots", getPicNo() +"_"+ name);
             FileUtils.copyFile(scrFile, testScreenshot);
             System.out.println("Screenshot stored to " + testScreenshot.getAbsolutePath());
         } catch (IOException e) {
@@ -55,8 +94,22 @@ public class SafariSimulatorTest {
         }
     }
 
-    @After
-    public void tearDown() throws Exception {
+    private static int getPicNo() {
+        return ++picNo;
+    }
+
+    /**
+     * For resetting the app between tests one should use the
+     * resetApp() method. Not really needed for web apps when one can
+     * always reload the page to start from scratch.
+     */
+    // @After
+    // public void afterTest() throws Exception {
+    //     driver.resetApp();
+    // }
+    
+    @AfterClass
+    public static void tearDown() throws Exception {
         driver.quit();
     }
 }
